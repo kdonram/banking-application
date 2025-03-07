1412d7d33cd27e1e9fb134bc2b93f4587b46c487
kdrm <kdonram@gmail.com>
2025-03-07 16:15:44 +0530

    add the attributes and create a constructor in bank account class.

--- src/main/java/org/example/BankAccount.java
+++ src/main/java/org/example/BankAccount.java
@@ -1,4 +1,18 @@
 package org.example;
 
 public class BankAccount {
+
+    public String name;
+    public String birthday;
+    public String address;
+    public String accountNumber;
+    public String accountType;
+
+    public BankAccount(String name, String birthday, String address, String accountNumber, String accountType){
+        this.name = name;
+        this.birthday = birthday;
+        this.address = address;
+        this.accountNumber = accountNumber;
+        this.accountType = accountType;
+    }
 }
