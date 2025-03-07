5def8817a25023a45e9afb968100b5a8c5f58ce2
kdrm <kdonram@gmail.com>
2025-03-07 16:04:58 +0530

    Create AccountHolder class with attributes name, birthday, and address.

--- src/main/java/org/example/AccountHolder.java
+++ src/main/java/org/example/AccountHolder.java
@@ -1,4 +1,14 @@
 package org.example;
 
 public class AccountHolder {
+
+    public String name;
+    public String birthday;
+    public String address;
+
+    public AccountHolder(String name, String birthday, String address){
+        this.name = name;
+        this.address = address;
+        this.birthday = birthday;
+    }
 }
